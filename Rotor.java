fc3f166b6e0c271cc6a577f4b1b83dd95b3b01cc
Anna Kim <annakim@Annas-MacBook-Air.local>
2023-09-18 15:15:42 -0400

    Rotor commit

--- Rotor.java
+++ Rotor.java
@@ -9,21 +9,34 @@ public class Rotor {
         this.startChar = c;
         
         while(!this.rotate());
-            
+
     }
     
     public boolean rotate(){
         //TODO
-               
+        rotorValues = rotorValues.substring(1,rotorValues.length()) + rotorValues.charAt(0);
+
+        if(rotorValues.charAt(0)==(startChar)){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
     
 
     public int indexOf(char c){
         //TODO
+        for(int i = 0; i<rotorValues.length(); i++){
+            if(this.rotorValues.charAt(i) == c){
+                return i;
+            }
+        }
     }
 
     public char charAt(int idx){
         //TODO
+        return charAt(idx);
     }
 }
     
