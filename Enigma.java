2c70d1488342d12eef347857543efaac8bff2cce
Anna Kim <annakim@Annas-MacBook-Air.local>
2023-09-19 15:05:01 -0400

    Another commit

--- Enigma.java
+++ Enigma.java
@@ -25,7 +25,20 @@ public class Enigma{
         String middle = rotorInit[1];
         String out = rotorInit[2];
 
-        
+        String secretMessage = " ";
+        for(int i = 0; i<out.length(); i++){
+            if(message.charAt(0) == out.charAt(i)){
+                char find = middle.charAt(i);
+                for(int j = 0; j<out.length(); j++){
+                    if(find == out.charAt(j)){
+                        char findOut = inner.charAt(j);
+                        secretMessage += findOut;
+                        rotate(); //counterclockwise?
+                        break;
+                    }
+                }
+            }
+        }
 
         // rotate inner counterclockwise
         // find the letter in the outer line
@@ -34,29 +47,30 @@ public class Enigma{
         // align with inner
         //for look length = lenght of string 
 
+     //   return secretMessage;
     }
-
-
     
     public String encrypt(String message){
         //TODO
         String inner = rotorInit[0];
         String middle = rotorInit[1];
         String out = rotorInit[2];
-        String newMessage = "";
+        String secretMessage = " ";
+
         for(int i = 0; i<inner.length();i++){
             if(message.charAt(0) == inner.charAt(i)){
                 char find = out.charAt(i);
                 for(int j = 0; j<middle.length();j++){
                     if(find == middle.charAt(j)){
                         char findOut = out.charAt(j);
-                        newMessage += out.charAt(j);
+                        secretMessage += out.charAt(j);
+                        rotate();
+                        break;
                     }
                 }
             }
 
         }
-        rotate();
     
     private void rotate(){
         if(rotors[0].rotate()){
@@ -65,5 +79,7 @@ public class Enigma{
             }
         }
     }
-    
+//    return secretMessage;
 }
+
+
